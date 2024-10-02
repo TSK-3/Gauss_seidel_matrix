e38de2e953531502d5af80c4a89853204e88da1a
TSK-3 <thekarthiktangirala@gmail.com>
2024-10-02 16:42:52 +0530

    Update Guass_scidel.c

--- Guass_scidel.c
+++ Guass_scidel.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include<math.h>
 
-float approximationx(float a1, float a2, float a3, float b1, float b2, float b3, 
+float approximation_x(float a1, float a2, float a3, float b1, float b2, float b3, 
                     float c1, float c2, float c3, float d1, float d2, float d3, 
                     float y, float z);
-float approximationy(float a1, float a2, float a3, float b1, float b2, float b3, 
+float approximation_y(float a1, float a2, float a3, float b1, float b2, float b3, 
                     float c1, float c2, float c3, float d1, float d2, float d3, 
                     float x, float z);
-float approximationz(float a1, float a2, float a3, float b1, float b2, float b3, 
+float approximation_z(float a1, float a2, float a3, float b1, float b2, float b3, 
                     float c1, float c2, float c3, float d1, float d2, float d3, 
                     float x, float y);
 
@@ -23,25 +24,16 @@ int main() {
     scanf("%f%f%f%f", &a3, &b3, &c3, &d3);
     
     float x = 0.0, y = 0.0, z = 0.0,diff1=1,diff2=1,diff3=1,x1,y1,z1;
-    while(diff1 >= 0.001 && diff2 >= 0.001 && diff3 >=0.001){
+    while(abs(diff1) >= 0.001 && abs(diff2) >= 0.001 && abs(diff3) >=0.001){
         x1 = x;
         y1 = y;
         z1 = z;
-        x = approximationx(a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, y, z);
-        y = approximationy(a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, x, z);
-        z = approximationz(a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, x, y);
+        x = approximation_x(a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, y, z);
+        y = approximation_y(a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, x, z);
+        z = approximation_z(a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3, x, y);
         diff1=x1-x;
         diff2=y1-y;
         diff3=z1-z;
-        if(diff1<0){
-            diff1 = -diff1;
-        }
-        if(diff2<0){
-            diff2 = -diff2;
-        }
-        if(diff3<0){
-            diff3 = -diff3;
-        }
     }
     
     printf("Values: x = %f, y = %f, z = %f\n", x, y, z);
